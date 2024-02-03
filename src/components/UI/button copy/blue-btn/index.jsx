9dfcc0b4e010949b164ebb392a88facf4d41d225
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2024-02-03 20:11:23 +0500

    delete button copy

--- src/components/UI/button copy/blue-btn/index.jsx
+++
@@ -1,9 +0,0 @@
-import cls from './bluebtn.module.scss'
-
-export default function BlueBtn({ white, children, type = "button", className, ...other }) {
-    return (
-        <button type={type} className={`${cls.GlobalBtn} ${white ? cls.GlobalBtn__white : ""} ${className && className}`} {...other}>
-            {children}
-        </button>
-    )
-}
--- src/components/UI/button copy/global-btn/index.jsx
+++
@@ -1,9 +0,0 @@
-import cls from './global.module.scss'
-
-export default function GlobalBtn({ children, type = "button", className, ...other }) {
-    return (
-        <button type={type} className={`${cls.GlobalBtn} ${className && className}`} {...other}>
-            {children}
-        </button>
-    )
-}
